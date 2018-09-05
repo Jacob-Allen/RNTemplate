74e5518d7a6f4b829c4226510a79e161c22643d6
Jacob Allen <jacobrallen1@gmail.com>
2018-09-05 16:56:42 -0500

    chore(build): added launch icons to android platform setup

--- dist/tools/android_config/setup_signing_identity.sh
+++ dist/tools/android_config/setup_signing_identity.sh
@@ -9,3 +9,11 @@ rm ../../../android/build.gradle
 cp top_build.gradle ../../../android/build.gradle
 rm ../../../android/gradle/wrapper/gradle-wrapper.properties
 cp gradle-wrapper.properties ../../../android/gradle/wrapper
+rm ../../../android/app/src/main/res/mipmap-hdpi/ic_launcher.png
+cp res/mipmap-hdpi/ic_launcher.png ../../../android/app/src/main/res/mipmap-hdpi
+rm ../../../android/app/src/main/res/mipmap-mdpi/ic_launcher.png
+cp res/mipmap-mdpi/ic_launcher.png ../../../android/app/src/main/res/mipmap-mdpi
+rm ../../../android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
+cp res/mipmap-xhdpi/ic_launcher.png ../../../android/app/src/main/res/mipmap-xhdpi
+rm ../../../android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
+cp res/mipmap-xxhdpi/ic_launcher.png ../../../android/app/src/main/res/mipmap-xxhdpi
